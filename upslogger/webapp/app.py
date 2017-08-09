2f8099316d429791db1f1d659943b5a40cbcf210
nocarryr <matt@nomadic-recording.com>
2017-08-09 13:04:20 -0500

    Add date range slider

--- upslogger/webapp/app.py
+++ upslogger/webapp/app.py
@@ -1,4 +1,5 @@
 import os
+import datetime
 os.environ.update({
     #'BOKEH_DEV':'true',
     'BOKEH_MINIFIED':'false',
@@ -7,7 +8,8 @@ os.environ.update({
     'BOKEH_PRETTY':'true',
 })
 from bokeh.layouts import column, row, widgetbox
-from bokeh.models import ColumnDataSource, HoverTool, Button, CustomJS
+from bokeh.models import ColumnDataSource, HoverTool, Button, CustomJS, Range1d
+from bokeh.models.widgets import DateRangeSlider
 from bokeh.plotting import figure, curdoc
 
 from upslogger.logger import parse_logfile
@@ -71,9 +73,12 @@ hover = HoverTool(
     mode='vline',
 )
 
+now = timezone.now('UTC').replace(tzinfo=None)
+DT_RANGE = (now - datetime.timedelta(days=7), now)
+
 p1 = figure(
     x_axis_type='datetime', plot_width=400, plot_height=350,
-    toolbar_location='right',
+    toolbar_location='right', x_range=Range1d(*DT_RANGE),
     sizing_mode='stretch_both',
 )
 p1.add_tools(hover)
@@ -96,7 +101,7 @@ p2.legend.location = 'top_left'
 p2.xaxis.axis_label = 'Date'
 p2.yaxis.axis_label = 'Frequency'
 
-callback = CustomJS(args=dict(source=data_src), code="""
+source_js_callback = CustomJS(args=dict(source=data_src), code="""
     var now = new Date(),
         utcOffset = now.getTimezoneOffset() * 60 * 1000;
     for (i=0; i<source.data.date.length; i++){
@@ -107,11 +112,55 @@ callback = CustomJS(args=dict(source=data_src), code="""
     console.log(source.data);
 
 """)
-data_src.js_on_change('stream', callback)
+data_src.js_on_change('stream', source_js_callback)
 
 btn = Button(label='Update')
 btn.on_click(update_data_src)
-wid_box = widgetbox(btn, sizing_mode='scale_width')
+
+
+
+date_widget = DateRangeSlider(
+    title='Date Range',
+    value=DT_RANGE,
+    start=DT_RANGE[0],
+    end=DT_RANGE[1],
+)
+
+def source_py_callback(attr, old, new):
+    if not len(data_src.data['date']):
+        return
+    min_dt = min(data_src.data['date'])
+    max_dt = max(data_src.data['date'])
+    date_widget.start = min_dt
+    date_widget.end = max_dt
+    date_widget.value = (min_dt, max_dt)
+
+
+data_src.on_change('data', source_py_callback)
+
+# date_widget_js_callback = CustomJS(code="""
+#     var d0 = new Date(cb_obj.value[0]),
+#         d1 = new Date(cb_obj.value[1]);
+#     console.log(d0, d1);
+# """)
+# date_widget.js_on_change('value', date_widget_js_callback)
+
+def date_widget_py_callback(attr, old, new):
+    global DT_RANGE
+    _dt_range = []
+    for dt in new:
+        if not isinstance(dt, datetime.datetime):
+            dt = datetime.datetime.utcfromtimestamp(dt/1000.)
+        dt = dt.replace(tzinfo=None)
+        _dt_range.append(dt)
+    DT_RANGE = tuple(_dt_range)
+    p1.x_range.start = _dt_range[0]
+    p1.x_range.end = _dt_range[1]
+
+date_widget.on_change('value', date_widget_py_callback)
+
+
+wid_box = widgetbox(btn, date_widget, sizing_mode='scale_width')
 
 pl_row = row(p1, p2, sizing_mode='stretch_both')
 
